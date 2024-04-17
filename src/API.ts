5223ca0871d4ad3b2f8520f9c26b2437a53637e9
Facundo-B <fmblanco97@gmail.com>
2024-04-17 15:19:13 -0300

    API call setup and error handling

--- src/API.ts
+++ src/API.ts
@@ -1,9 +1,31 @@
+import { shuffleAnswers } from "./utils";
+
+
+export type Question = {
+    type: string;
+    question: string;
+    category: string;
+    difficulty: Difficulty;
+    correct_answer: string;
+    incorrect_answers: string[]
+}
+
+export type QuestionState = Question & { answers: string[]; }
 
 export type Difficulty = "easy" | "medium" | "hard"
 
-export const  fetchQuestions = async (amount: number, difficulty: Difficulty) => {
+export const fetchQuestions = async (amount: number, difficulty: Difficulty) => {
+
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&category=18&type=multiple`
     const data = await (await fetch(endpoint)).json()
-    console.log(endpoint)
-    console.log(data)
+    // console.log(endpoint)
+    // console.log(data)
+    if (data.response_code != 0) {
+        throw new Error("Api call failed");
+    } else {
+        return data.results.map((question: Question): Object => ({
+            ...question,
+            answers: shuffleAnswers([...question.incorrect_answers, question.correct_answer])
+        }))
+    }
 }
---
+++ src/utils.ts
@@ -0,0 +1,1 @@
+export const shuffleAnswers = (answers: any[]): any[] => [...answers].sort(() => Math.random() - 0.5);
