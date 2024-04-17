cf6ee5a5f0a9527641bc8fbe52d62a6d1228867a
Facundo-B <fmblanco97@gmail.com>
2024-04-17 18:01:40 -0300

    further logic functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
 import { useState } from "react"
+import { QuestionState, fetchQuestions } from "./API"
+
+type Answer = {
+  question: string;
+  userAnswer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+//Components
 import QuestionCard from "./components/QuestionCard"
-import { fetchQuestions} from "./API"
 
 function App() {
 
   const totalQuestions: number = 12
 
   const [loading, setLoading] = useState(false)
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState<QuestionState[]>([])
   const [questionNbr, setQuestionNbr] = useState(0)
-  const [userAnswers, setUserAnswers] = useState([])
+  const [userAnswers, setUserAnswers] = useState<Answer[]>([])
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true)
 
-  console.log(fetchQuestions(totalQuestions, "easy"))
+  const startTrivia = async (): Promise<void | string> => {
+    //API call
+    setLoading(true);
+    setGameOver(false);
+    try {
+      const newQuestions = await fetchQuestions(totalQuestions, "medium")      
+      setQuestions(newQuestions);
+      setScore(0);
+      setUserAnswers([]);
+      setQuestionNbr(0);
+      setLoading(false)
+    } catch (error: any) {
+      return error instanceof Error ? error.message : error
+    }
 
-  const startTrivia = async () => {
-    //API call  
   }
 
   const checkAnswer = (e: React.MouseEvent): void => {
@@ -31,18 +51,20 @@ function App() {
     <main className="relative">
       <section>
         <h1>CompQuiz</h1>
-        <button onClick={startTrivia}>Start</button>
-        <p>Score</p>
-        <p>Loading Questions...</p>
-        <QuestionCard
-        questionNbr={questionNbr + 1}
-        totalQuestions={totalQuestions}
-        question = {questions[questionNbr].question}
-        answers={questions[questionNbr].answer}
-        userAnswer={userAnswers ? userAnswers[questionNbr] : undefined}
-        callback={checkAnswer}
-        />
-        <button onClick={nextQuestion}>Next Question</button>
+        {
+          (gameOver || userAnswers.length === totalQuestions) && <button onClick={startTrivia}>Start</button>
+        }
+        {!gameOver && <p>Score</p>}
+        {loading && <p>Loading Questions...</p>}
+        {/* <QuestionCard
+          questionNbr={questionNbr + 1}
+          totalQuestions={totalQuestions}
+          question={questions[questionNbr].question}
+          answers={questions[questionNbr].answers}
+          userAnswer={userAnswers ? userAnswers[questionNbr] : undefined}
+          callback={checkAnswer}
+        /> */}
+        <button className="" onClick={nextQuestion}>Next Question</button>
       </section>
     </main>
   )
